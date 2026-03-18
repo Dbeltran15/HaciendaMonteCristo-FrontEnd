073405dc9f09eeb43f1019d303c8a7e6c6867b67
rajuria <rajuria@unitec.edu>
2026-03-18 15:05:29 -0600

    Vite Instalado
    Se instalo Vite y se hicieron algunos cambios al App principal

---
+++ HaciendaMontecristo/src/App.jsx
@@ -0,0 +1,97 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import swaggerIcon from './assets/swagger-api-icon.svg'
+import TrelloIcon from './assets/trello-logo-icon.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Proyecto Hacienda Monte Cristo</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={swaggerIcon} alt="Swagger logo" />
+                Swagger
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <ul>
+            <li>
+              <a href="https://github.com/rajuria/HaciendaMonteCristo-FrontEnd/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub - Front End
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/rajuria/HaciendaMonteCristo-BackEnd" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub - Back End
+              </a>
+            </li>
+            <li>
+              <a href="https://trello.com/b/TEdEdWl3/ux-473-hacienda-montecristo" target="_blank">
+                <img className="button-icon" src={TrelloIcon} alt="Trello logo" />
+                Trello
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
